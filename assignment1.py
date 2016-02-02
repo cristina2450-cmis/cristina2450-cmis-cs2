b3aef507ca6946c4cff8d8f52ccb8eae4758d79f
cmis <cmis@cmis-hs18.(none)>
2016-02-02 12:02:24 +0700

    assignment1

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+myname = "Cris"
+myage = 16
+myheightinmeters = 1.56
+lengthofsquare = 2
+lengthofrectangle = 5
+heightofrectangle = 3
+myageinmonths = myage * 12
+yearslefttolive = 80 - myage
+myheightinfeet = myheightinmeters * 3.28084
+differenceinaverageheight = 1.62 - myheightinmeters
+areaofsquare = lengthofsquare ** 2
+volumeofcube = lengthofsquare ** 3 / 2
+ninthofrectanglearea = lengthofrectangle * heightofrectangle / 9
+print "Hi. " + "I'm " + str(myname) + "."
+print "I'm " + str(myage) + " years old."
+print "That is " + str(myageinmonths) + " months."
+print "I am " +str(myheightinmeters) + " meters tall."
+print "That is equal to " + str(myheightinfeet) + " feet."
+print "My height is", myheightinmeters, " meters." 
+print "That is", differenceinaverageheight, " meters shorter than average."
+print "I am", myage,"years old."
+print "That is", myageinmonths, "months."
+print "I have about", yearslefttolive,"years to live."
+print ";)"
+
