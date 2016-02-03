9f2f904f52107e53c02c96f2b9066d29cd23cd09
cmis <cmis@cmis-hs18.(none)>
2016-02-03 11:48:30 +0700

    finished assignment

--- assignment1.py
+++ assignment1.py
@@ -1,25 +1,25 @@
-myname = "Cris"
-myage = 16
-myheightinmeters = 1.56
-lengthofsquare = 2
-lengthofrectangle = 5
-heightofrectangle = 3
-myageinmonths = myage * 12
-yearslefttolive = 80 - myage
-myheightinfeet = myheightinmeters * 3.28084
-differenceinaverageheight = 1.62 - myheightinmeters
-areaofsquare = lengthofsquare ** 2
-volumeofcube = lengthofsquare ** 3 / 2
-ninthofrectanglearea = lengthofrectangle * heightofrectangle / 9
-print "Hi. " + "I'm " + str(myname) + "."
-print "I'm " + str(myage) + " years old."
-print "That is " + str(myageinmonths) + " months."
-print "I am " +str(myheightinmeters) + " meters tall."
-print "That is equal to " + str(myheightinfeet) + " feet."
-print "My height is", myheightinmeters, " meters." 
-print "That is", differenceinaverageheight, " meters shorter than average."
-print "I am", myage,"years old."
-print "That is", myageinmonths, "months."
-print "I have about", yearslefttolive,"years to live."
-print ";)"
+myname = "Cris" # assigns Cris to my name
+myage = 16 # assigns 16 to my age
+myheightinmeters = 1.56 # assigns 1.56 to the variable
+lengthofsquare = 2 # assigns 2 to the variable
+lengthofrectangle = 5 # assigns 5 to the variable
+heightofrectangle = 3 # assigns 3 to the variable
+myageinmonths = myage * 12 # calculates my age in months
+yearslefttolive = 80 - myage # calculates how many years to live
+myheightinfeet = myheightinmeters * 3.28084 # calculates my height in feet
+differenceinaverageheight = 1.62 - myheightinmeters # calculates the difference in average height
+areaofsquare = lengthofsquare ** 2 # calculates area of square
+volumeofcube = lengthofsquare ** 3 / 2 # calculates half the volume of a cube
+ninthofrectanglearea = lengthofrectangle * heightofrectangle / 9 # calculates a ninth of a rectangle's area
+print "Hi. " + "I'm " + str(myname) + "." # prints a sentence
+print "I'm " + str(myage) + " years old."# prints a sentnce
+print "That is " + str(myageinmonths) + " months." # prints a sentence
+print "I am " +str(myheightinmeters) + " meters tall." # prints a sentence
+print "That is equal to " + str(myheightinfeet) + " feet." # prints a sentence
+print "My height is", myheightinmeters, " meters." # prints a sentence
+print "That is", differenceinaverageheight, " meters shorter than average." # prints a sentence
+print "I am", myage,"years old." # prints a sentence
+print "That is", myageinmonths, "months." # prints a sentence
+print "I have about", yearslefttolive,"years to live." # prints a sentence
+print ";)"*10000 # prints 10,000 winky faces
 
