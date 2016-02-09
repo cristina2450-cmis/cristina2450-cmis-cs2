a4d9645d0b39d3da150695fbc9763f7fb4e4b04a
cmis <cmis@cmis-hs18.(none)>
2016-02-09 14:04:00 +0700

    functions assignment

---
+++ functions.py
@@ -0,0 +1,25 @@
+def add(a, b):
+	return a + b
+
+def sub(a, b):
+	return a - b
+
+def mul(a, b):
+	return a * b
+
+def div(a, b):
+	return a / b
+ 
+def hours_from_second(seconds):
+	return seconds/3600
+
+def circle_area(radius):
+	return math.pi(radius)**2
+
+def sphere_volume(radius):
+	return 4/3 * math.pi(radius)**3
+
+def avg_volume(a, b):
+	return  ((4/3 * math.pi(a)**3)+(4/3 * math.pi(b)**3))/2
+
+
