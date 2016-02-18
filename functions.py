e3c970852121919bb04f4899facffe27f9b4b87b
cmis <cmis@cmis-hs18.(none)>
2016-02-18 11:55:51 +0700

    getting there

--- functions.py
+++ functions.py
@@ -3,40 +3,74 @@ import math
 def add(a, b):# takes two numbers and returns their sum
 	return a + b
 
-add(3, 4)
+a = add(3, 4)
+b = add(5, 6)
+
 def sub(a, b):# subtracts second number from first
 	return a - b
+
+c = sub(9, 5)
+d = sub(7, 8)
 
 def mul(a, b):# mulitiplies two numbers together
 	return a * b
 
+e = mul(3 ,4)
+f = mul(2, 6)
+
 def div(a, b):# divides first number by second
 	return a / b
- 
+
+g = div(9, 3)
+h = div(4, 2)
+
 def hours_from_second(seconds):# converts seconds to hours
 	return seconds/3600
 
+i = hours_from_seconds(7200)
+j = hours_from_seconds(9000)
+
 def circle_area(radius):# finds area of circle
-	return math.pi(radius)**2
+	return math.pi * (radius**2)
+
+k = circle_area(3)
+l = circle_area(5)
 
 def sphere_volume(radius):# finds volume of sphere
-	return 4/3 * math.pi(radius)**3
+	return 1.333333333 * math.pi * (radius**3)
+
+m = sphere_volume(6)
+n = sphere_volume(10)
 
 def avg_volume(a, b):# finds averge volume of two spheres
-    average = ((4/3 * math.pi(a)**3)+(4/3 * math.pi(b)**3))/2
-    float(average)  
-    return average
+    ha = a/2
+    hb = b/2
+    
 
-avg_volume (10, 20)
+o = avg_volume (10, 20)
+p = avg_volume (5, 7)
+
 def area(a, b, c):# finds area of triangle when given 3 sides
 	p =(a + b + c)/2
 	return (p*(p-a)*(p-b)*(p-c))**0.5
 
+q = area(12, 14, 3)
+r = area(10, 5, 7)
+
 def right_align(word):# aligns word to the right
     return (80 -len(word)*("   ") + word
+
+s = right_align("hello")
+t = right_align("blah")
 
 def center(word):# aligns word to center
     return (40 -len(word)*("   ") + word
 
-def msg_box(message):
+u = center("python")
+v = center("functions")
 
+def msg_box(word):
+    return"+" + ((len(word)+4)*"-")+"+"+"\n"+"|"+(2*"")+(word)+(2*"")+"|"+"\n"+"+"+((len(word)+4)*"-")+"+"
+
+w = msg_box(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v)
+print w
