d6da413da8a085bbca31501ef58e357c233bd303
cmis <cmis@cmis-hs18.(none)>
2016-03-03 11:54:10 +0700

    marking

--- cs2quiz1.py
+++ cs2quiz1.py
@@ -1,17 +1,17 @@
 #Part 1: Terminology (15 points)
-#1 1pt) What is the symbol "=" used for?
+#1 1pt) What is the symbol "=" used for? - (1pt)
 # It is used to assign a value to a variable.
 #
 #
-#2 3pts) Write a technical definition for 'function'
+#2 3pts) Write a technical definition for 'function' - (2pts)
 # A function is a series of steps used to solve a problem.
 #
 #
-#3 1pt) What does the keyword "return" do?
+#3 1pt) What does the keyword "return" do? - (1pt)
 # It gives back whatever follows it.
 #
 #
-#4 5pts) We know 5 basic data types. Write the name for each one and provide two
+#4 5pts) We know 5 basic data types. Write the name for each one and provide two - (5pts)
 #   examples of each below
 #	1: interger 1, 2
 #	2: float 1.2, 2.3
@@ -20,13 +20,13 @@
 #	5: tuple, ("hello", 35), ("world", 546, True)
 #
 #5 2pts) What is the difference between a "function definition" and a 
-#        "function call"?
+#        "function call"? - (2pts)
 # A function definition is the job the function must carry out.
 # A function call is giving the function an argument which it applies to
 # what it has to do.
 #
 #6 3pts) What are the 3 phases that every computer program has? What happens in
-#        each of them
+#        each of them - (1pt)
 #	1: input
 #	2: process
 #	3: output
@@ -47,6 +47,11 @@
 
 import math
 
+#1 pt for header line - (1pt)
+#3 pt for correct formula - (1pts)
+#1 pt for return value - (0pts)
+#1 pt for parameter name - (0pts)
+#1 pt for function name - (1pt)
 def diameter_of_circles(a, b, c):
     a = raw_input("area of circle 1 >>> ")
     b = raw_input("area of circle 2 >>> ")
@@ -62,6 +67,23 @@ def diameter_of_circles(a, b, c):
     print db
     print dc
     print total
+#1pt for header line
+#1pt for parameter names
+#1pt for return value
+#1pt for correct output format
+#3pt for correct use of format function
+
+#1pt header line
+#1pt getting input
+#1pt converting input
+#1pt for calling output function
+#2pt for correct diameter formula
+#1pt for variable names
+
+#1pt for calling main
+
+#1pt explanatory comments
+#1pt code format
 
 p = diameter_of_circles(1, 2, 3)
 
