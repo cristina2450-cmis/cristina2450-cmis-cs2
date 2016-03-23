34ba2664e0e564cbada0334c685995b72469ac5b
cmis <cmis@cmis-hs13.(none)>
2016-03-23 11:49:42 +0700

    so far

---
+++ conditionals.py
@@ -0,0 +1,17 @@
+# You are walking down the street and you come across a cafe.
+# You are asked if you'd like to enter.
+
+# If you enter you are asked what you want to order.
+#   If you order juice you spill it on your shirt
+#   If you order something something else happens
+#   If you order a sandwich you happily leave on your way
+#   If you order a coffee you burn your tongue.
+# Eventually you're on your way again
+
+# If you keep going you come to the street corner
+# If you turn left 
+#   You come across a robber and are asked to fight or flee 
+#   If you fight
+#   If you flee
+# If you turn right
+#   Then this happens
