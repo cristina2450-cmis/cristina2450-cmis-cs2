a0d5f08181806041c1c73413c2a56c42b038057d
cmis <cmis@cmis-hs13.(none)>
2016-05-06 11:51:05 +0700

    working

---
+++ newguess.py
@@ -0,0 +1,36 @@
+import math
+import random
+from random import randint
+
+def whole_game():
+    
+    actual_number = random.randint(1, 100)
+
+    def game():
+    
+        points = 0
+        guess = int(raw_input("number: "))
+    
+        if guess == actual_number:
+            print "you're right."
+            points = points + 1
+            print "you've got", points, "point(s)."
+
+        if guess > actual_number:
+            print "that's too high."
+            game()
+
+        if guess < actual_number:
+            print "that's too low."
+            game()
+
+    game()
+
+
+def main():
+
+    whole_game()
+    whole_game()
+    whole_game()
+
+main()
