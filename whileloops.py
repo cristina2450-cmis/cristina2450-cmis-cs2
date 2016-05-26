4d903aaff2464935a3bf58d5c3ceef7203a4e2ce
cmis <cmis@cmis-hs13.(none)>
2016-05-26 14:33:11 +0700

    working

---
+++ whileloops.py
@@ -0,0 +1,25 @@
+
+#x = 0
+
+#while x < 10:
+#	print x
+#	x += 1
+
+#y = 10
+
+#while y >= 0:
+#	print y
+#	y -=1
+
+def counter(x):
+	if x >= 0:
+		while x >= 0:
+			print x
+			x -= 1
+
+	if x <= 0:
+		while x <=0:
+			print x
+			x += 1
+counter(5)
+
